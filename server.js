b5855018975ce9dd710939daa56d3b3825cd2277
Varun <varutalk@gmail.com>
2021-05-07 01:15:00 +0530

    First Commit : Folder structure and express server set up

---
+++ server.js
@@ -0,0 +1,13 @@
+const express=require('express')
+const app=express()
+const port=5000
+
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(`Error : ${err}`)
+    }
+    else{
+        console.log(`listening at http://localhost:${port}`)
+    }
+})
