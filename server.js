04ff585555b0d01ba27ca35b211908f6369e1b5a
Varun <varutalk@gmail.com>
2021-05-09 00:26:14 +0530

    entry route and home_controller

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app=express()
 const port=5000
 
-
+app.use('/',require('./routes/index'))
 app.listen(port,(err)=>{
     if(err){
         console.log(`Error : ${err}`)
