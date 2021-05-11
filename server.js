89fd74cd015005a0cd2a25d42bdd3b9fac4bee94
Varun <varutalk@gmail.com>
2021-05-11 17:00:48 +0530

    used scss and made header and footer

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const port=5000
 const expressSession=require('express-session')
 const passport=require('./config/passport_set_up')
 const MongoStore = require('connect-mongo')//MongoDB to store user's session 
+const sassMiddleware = require('node-sass-middleware')//using sass middleware
 
 require('./config/hbs_config').hbsConfig(app)//handlebars configuration
 require('./config/db_conn')// db connection
 
+//body parser
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))//body parser
+app.use(express.urlencoded({extended:true}))
 
-app.use(expressSession({//express-session set up
+//express-session set up
+app.use(expressSession({
     saveUninitialized:false,
     resave:false,
     secret:'some long string',
@@ -24,10 +27,23 @@ app.use(expressSession({//express-session set up
         autoRemove : 'disabled'
     })
 }))
-
+//passport middlewares
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(passport.setAuthenticatedUser)
+
+//sass middleware
+app.use(sassMiddleware({
+    /* Options */
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix:  '/css'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+}));
+// Note: you must place sass-middleware *before* `express.static` or else it will
+// not work.
+app.use(express.static(__dirname+'/assets'))
 
 app.use('/',require('./routes/index'))
 
