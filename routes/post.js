88d2bbb4c4f7a8809082d19ed678b48a662c00c5
Varun <varutalk@gmail.com>
2021-05-13 18:08:18 +0530

    authentication for creating post

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,8 @@
 const express=require('express')
+const passport = require('passport')
 const router=express.Router()
 const postController=require('../controllers/post_controller')
 
-router.post('/create',postController.create)
+router.post('/create',passport.checkAuthentication,postController.create)
 
 module.exports=router
